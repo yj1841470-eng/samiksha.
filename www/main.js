055269bbf34005fa5bce498a75292e7930448e1f
yj1841470-eng <yj1841470@gmail.com>
2025-09-12 07:46:34 +0530

    Hood design done

---
+++ www/main.js
@@ -0,0 +1,13 @@
+$(document).$(document).ready(function () {
+    
+    $('.text').textillate({
+        loop: true,
+        sync:true,
+        in:{
+            effect: "bounceIn",
+        },
+        out:{
+            effect:"bounceOut",
+        },
+    })
+});
